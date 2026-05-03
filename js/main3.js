79bdb545744d79107fa7fb3543b123ae9288f8ac
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-03 19:15:57 +0300

    İlk olarak genel tutucu tanımlandı, sonrasında string verilerimi array haline getirdim

--- js/main3.js
+++ js/main3.js
@@ -1,49 +1,11 @@
 let pricing = [];
 
-window.onload = function () {
+window.onload = function(){
   const data = localStorage.getItem("pricing");
 
-  if (data) {
+  if (data){
     pricing = JSON.parse(data);
   }
 
   renderPrice();
-};
-
-// EKLEME FONKSİYONU
-function addPrice() {
-  const input = document.getElementById("money");
-  const value = input.value;
-
-  if (value === "") return;
-
-  pricing.push(value);
-  localStorage.setItem("pricing", JSON.stringify(pricing));
-
-  renderPrice();
-  input.value = "";
-}
-
-// EKRANA BASMA (RENDER)
-function renderPrice() {
-  const list = document.getElementById("priceList");
-
-  list.innerHTML = "";
-
-  pricing.forEach(function (value) {
-    const li = document.createElement("li");
-    li.textContent = value;
-
-    const btn = document.createElement("button");
-    btn.textContent = "SİL";
-
-    btn.onclick = function () {
-      pricing = pricing.filter((p) => p !== value);
-      localStorage.setItem("pricing", JSON.stringify(pricing));
-      renderPrice();
-    };
-
-    li.appendChild(btn);
-    list.appendChild(li);
-  });
 }

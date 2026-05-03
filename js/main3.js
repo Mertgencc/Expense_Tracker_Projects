2b19d5d202f03c0e6a2a984c997f81f235970d67
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-03 19:31:37 +0300

    Son olarak ekrana render ettirme yani ekrana çizdirme yaptırdım

--- js/main3.js
+++ js/main3.js
@@ -25,3 +25,24 @@ function addPrice(){
 
 }
 
+function renderPrice(){
+  const list = document.getElementById("priceList");
+  list.innerHTML = "";
+
+  pricing.forEach(function(value){
+    const li = document.createElement("li");
+    li.textContent = value;
+
+    const btn = document.createElement("button");
+    btn.textContent = "SİL";
+
+    btn.onclick = function(){
+      pricing = pricing.filter((p) => p !== value);
+      localStorage.setItem("pricing", JSON.stringify(pricing));
+      renderPrice();
+    };
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  })
+}
